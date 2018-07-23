fa370fa098ace01b65047c6c08c120d1f8e6a913
en-programmerare <38755423+en-programmerare@users.noreply.github.com>
2018-07-23 11:57:07 +0200

    something

---
+++ test2.js
@@ -0,0 +1,23 @@
+var MyExtension = function () {
+};
+MyExtension.prototype.testblock = function() {
+    alert("it's working");
+};
+
+MyExtension.prototype.getInfo = function () {
+    return {
+        id: "test",
+        name: "My test addon",
+        
+        blocks: [
+            {
+                opcode: "testblock",
+                text: "Do something",
+                blockType: BlockType.COMMAND,
+                arguments: {}
+            }
+        ]
+    };
+};
+
+Scratch.extensions.register(new MyExtension());
