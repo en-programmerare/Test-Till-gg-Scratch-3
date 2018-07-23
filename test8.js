4bd309f7c699607c884b3a9cae7832bde0d0fac0
en-programmerare <38755423+en-programmerare@users.noreply.github.com>
2018-07-23 17:36:09 +0200

    Create test8.js

---
+++ test8.js
@@ -0,0 +1,107 @@
+var alarmHour = -1;
+var alarmMinute = -1;
+
+var MyExtension = function () {
+};
+
+MyExtension.prototype.setAlarmMidday = function(args) {
+    if(args.MIDDAY === "midday") {
+        alarmMinute = 0;
+        alarmHour = 12;
+    }
+    else {
+        alarmMinute = 0;
+        alarmHour = 0;
+    }
+};
+
+MyExtension.prototype.getAlarm = function(args) {
+    if(alarmHour === -1)
+        return "Alarm is off";
+    else {
+        if(args.TYPE === "time") {
+            return alarmHour + ":" + alarmMinute;
+        }
+        if(args.TYPE === "hour") {
+            return alarmHour;
+        }
+        return alarmMinute;
+    }
+};
+
+MyExtension.prototype.getAlarmOn = function(args) {
+    if(alarmHour === -1)
+        return false;
+    return true;
+};
+
+MyExtension.prototype.turnOff = function(args) {
+    alarmHour = -1;
+    alarmMinute = -1;
+};
+
+MyExtension.prototype.alarmEvent = function(args) {
+    Date time = new Date();
+    if(time.getHours() === alarmHour && time.getMinutes() === alarmMinute)
+        return true;
+    return false;
+};
+
+MyExtension.prototype.getInfo = function () {
+    return {
+        id: "alarm",
+        name: "Alarm",
+        
+        blocks: [
+            {
+                opcode: "setAlarmMidday",
+                text: "set alarm to [MIDDAY]",
+                blockType: Scratch.BlockType.COMMAND,
+                arguments: {
+                    MIDDAY: {
+                        type: Scratch.ArgumentType.STRING,
+                        defaultValue: "midday",
+                        menu: "middayMidnight"
+                    }
+                }
+            },
+            {
+                opcode: "getAlarm",
+                text: "get alarm [TYPE]",
+                blockType: Scratch.BlockType.REPORTER,
+                arguments: {
+                    TYPE: {
+                        type: Scratch.ArgumentType.STRING,
+                        defaultValue: "time",
+                        menu: "alarmFetchType"
+                    }
+                }
+            },
+            {
+                opcode: "getAlarmOn",
+                text: "alarm on?",
+                blockType: Scratch.BlockType.BOOLEAN,
+                arguments: {}
+            },
+            {
+                opcode: "turnOff",
+                text: "turn alarm off",
+                blockType: Scratch.BlockType.COMMAND,
+                arguments: {}
+            },
+            {
+                opcode: "alarmEvent",
+                text: "when alarm goes off",
+                blockType: Scratch.BlockType.HAT,
+                arguments: {}
+            }
+        ],
+        
+        menus: {
+            middayMidnight: ["midday", "midnight"],
+            alarmFetchType: ["time", "hour", "minute"]
+        }
+    };
+};
+
+Scratch.extensions.register(new MyExtension());
