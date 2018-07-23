e5f0a7bfb97a6ea9edddeea33793e68eda69f98e
en-programmerare <38755423+en-programmerare@users.noreply.github.com>
2018-07-23 17:06:36 +0200

    Added blocks

--- test2.js
+++ test2.js
@@ -1,19 +1,45 @@
+var alarmHour = -1;
+var alarmMinute = -1;
+
 var MyExtension = function () {
 };
-MyExtension.prototype.testblock = function() {
-    window.alert("it's working");
+
+MyExtension.prototype.setAlarmMidday = function() {
+    alarmMinute = 0;
+    alarmHour = 12;
+};
+
+MyExtension.prototype.setAlarmMidnight = function() {
+    alarmMinute = 0;
+    alarmHour = 0;
+};
+
+MyExtension.prototype.getAlarm = function() {
+    return alarmHour + ":" + alarmMinute;
 };
 
 MyExtension.prototype.getInfo = function () {
     return {
-        id: "test",
-        name: "My test addon",
+        id: "alarm",
+        name: "Alarm Extension",
         
         blocks: [
             {
-                opcode: "testblock",
-                text: "Do something",
+                opcode: "setAlarmMidday",
+                text: "set alarm to midday",
                 blockType: Scratch.BlockType.COMMAND,
+                arguments: {}
+            },
+            {
+                opcode: "setAlarmMidnight",
+                text: "set alarm to midnight",
+                blockType: Scratch.BlockType.COMMAND,
+                arguments: {}
+            },
+            {
+                opcode: "getAlarm",
+                text: "get alarm",
+                blockType: Scratch.BlockType.REPORTER,
                 arguments: {}
             }
         ]
