5c9167357130e7251474515577f54d38964dfdb1
en-programmerare <38755423+en-programmerare@users.noreply.github.com>
2018-07-23 11:54:39 +0200

    Added a block

--- test.js
+++ test.js
@@ -1,10 +1,22 @@
 var MyExtension = function () {
-    
 };
+MyExtension.prototype.testblock = function() {
+    alert("it's working");
+};
+
 MyExtension.prototype.getInfo = function () {
     return {
         id: "test",
-        name: "My test addon"
+        name: "My test addon",
+        
+        blocks: [
+            test: {
+                opcode: "testblock",
+                text: "Do something",
+                blockType: BlockType.COMMAND,
+                arguments: {}
+            }
+        ]
     };
 };
 
